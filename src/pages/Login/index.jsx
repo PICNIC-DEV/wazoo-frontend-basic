b2d7cfc5499c2c346b8b1c178203041b3bd5a367
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-07-25 21:54:30 +0900

    pages

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,139 @@
+import styled from 'styled-components';
+
+const ContainerMain = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f9f9f9;
+`;
+
+const ContainerLogoBox = styled.div`
+  width: 366px;
+  height: 115px;
+  padding: 10px;
+  gap: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+  margin-bottom: 48px;
+`;
+
+const ContainerLogoImage = styled.div`
+  display: flex;
+  width: 320px;
+  height: 62px;
+  justify-content: center;
+  align-items: center;
+  background-color: #87929a;
+`;
+
+const ContainerLogoTitle = styled.div`
+  color: var(--gray-60, #87929a);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const InputDataBox = styled.div`
+  display: flex;
+  width: 414px;
+  height: 24px;
+  padding: 20px 32px;
+  align-items: center;
+  gap: 10px;
+  border-radius: 55px;
+  border: 1px solid var(--gray-20, #d7dbdd);
+  background: #fff;
+  margin-top: 8px;
+`;
+
+const InputData = styled.div`
+  color: var(--gray-60, #87929a);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const ContainerButtons = styled.div`
+  width: 414px;
+  height: 64px;
+  gap: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  margin-top: 36px;
+`;
+
+const ButtonStyleLogin = styled.button`
+  display: flex;
+  width: 199px;
+  height: 64px;
+  padding: 8px 24px 8px 32px;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+  border-radius: 30px;
+  border-color: #27d562;
+  background: var(--primary, #27d562);
+  /* background-color: #27d562; */
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const ButtonStyleSignUp = styled.button`
+  display: flex;
+  width: 199px;
+  height: 64px;
+  padding: 8px 24px 8px 32px;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+  border-radius: 30px;
+  background: var(--gray-100, #374957);
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const Index = () => {
+  return (
+    <>
+      <ContainerMain>
+        <ContainerLogoBox>
+          <ContainerLogoImage>로고 이미지</ContainerLogoImage>
+          <ContainerLogoTitle>토박이 가이드 매칭 서비스</ContainerLogoTitle>
+        </ContainerLogoBox>
+        <InputDataBox>
+          <InputData>아이디를 입력하세요</InputData>
+        </InputDataBox>
+        <InputDataBox>
+          <InputData>비밀번호를 입력하세요</InputData>
+        </InputDataBox>
+        <ContainerButtons>
+          <ButtonStyleLogin>로그인</ButtonStyleLogin>
+          <ButtonStyleSignUp>회원가입</ButtonStyleSignUp>
+        </ContainerButtons>
+      </ContainerMain>
+    </>
+  );
+};
+
+export default Index;
--- src/routes/RouteDef.jsx
+++ src/routes/RouteDef.jsx
@@ -1,9 +1,24 @@
 import Main from '../pages/Main';
+import Login from '../pages/Login';
+import Mypage from '../pages/Mypage';
+import SignUp from '../pages/SignUp';
 
 const MainScreens = {
   Main: {
     path: '/',
     element: <Main />,
+  },
+  Login: {
+    path: '/login',
+    element: <Login />,
+  },
+  Mypage: {
+    path: '/mypage',
+    element: <Mypage />,
+  },
+  SignUp: {
+    path: '/signup',
+    element: <SignUp />,
   },
 };
 
