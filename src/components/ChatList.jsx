9d744f7c5b204f5bd4822ba7511097771599c113
shinhm1 <shinhm1@naver.com>
2024-07-26 17:38:54 +0900

    chatting

---
+++ src/components/ChatList.jsx
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const Item = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  padding: 18px 0;
+  align-items: center;
+  justify-content: space-between
+  border: 1px;
+
+  &::after {
+    position: absolute;
+    content: '';
+    width: 100%;
+    background-color: var(--border-color);
+    height: 1px;
+    bottom: 0;
+  }
+`;
+
+const LeftContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  align-items: center;
+  margin-left: 24px;
+`;
+
+const Profile = styled.div``;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding-left: 24px;
+  gap: 4px;
+`;
+
+const Name = styled.div`
+  color: var(--gray-100, #374957);
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const LastChat = styled.div`
+  color: var(--gray-60, #87929a);
+  font-size: 14px;
+  font-weight: 400;
+`;
+
+const DayTime = styled.div`
+  color: var(--gray-60, #87929a);
+  font-size: 14px;
+  font-weight: 400;
+  margin-right: 24px;
+  white-space: nowrap;
+`;
+
+// timestamp 포맷
+export const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  const today = new Date();
+
+  today.setHours(0, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
+
+  if (date.getTime() === today.getTime()) {
+    // 채팅 날짜가 오늘인 경우 시간만 표시
+    return new Date(timestamp.toLocaleTimeString());
+  } else {
+    // 채팅 날짜가 오늘 이전인 경우 날짜만 표시
+    return date.toLocaleDateString();
+  }
+};
+
+const ChatList = (props) => {
+  const item = props.item;
+  const { userId } = useParams();
+  const navigate = useNavigate();
+
+  // ChatList 클릭 시 해당 채팅방 입장
+  const handleChatClick = (item) => {
+    navigate(`/chat/${userId}/${item.chat_id}/${item.partner_name}`);
+  };
+
+  return (
+    <>
+      <Item
+        onClick={() => {
+          console.log(item.chat_id);
+          handleChatClick(item);
+        }}
+      >
+        <LeftContainer>
+          <Profile>
+            <div className="icon profile">
+              <svg className="icon-profile" />
+            </div>
+          </Profile>
+          <TextContainer>
+            <Name>{item.partner_name}</Name>
+            <LastChat>{item.message}</LastChat>
+          </TextContainer>
+        </LeftContainer>
+        <DayTime>{formatDate(item.create_at)}</DayTime>
+      </Item>
+    </>
+  );
+};
+
+export default ChatList;
--- src/routes/RouteDef.jsx
+++ src/routes/RouteDef.jsx
@@ -1,4 +1,7 @@
 import Main from '../pages/Main';
+import Chat from '../pages/ChatList';
+import ChatRoom from '../pages/ChatRoom';
+import Test from '../pages/Test';
 
 const MainScreens = {
   Main: {
@@ -7,6 +10,30 @@ const MainScreens = {
   },
 };
 
+const ChatListScreens = {
+  ChatList: {
+    path: '/chat/:userId',
+    element: <Chat />,
+  },
+};
+
+const ChatRoomScreens = {
+  ChatRoom: {
+    path: '/chat/:userId/:chatId/:partnerName',
+    element: <ChatRoom />,
+  },
+};
+
+const TestScreens = {
+  Test: {
+    path: '/test',
+    element: <Test />,
+  },
+};
+
 export const AppRouteDef = {
   ...MainScreens,
+  ...ChatListScreens,
+  ...ChatRoomScreens,
+  ...TestScreens,
 };
